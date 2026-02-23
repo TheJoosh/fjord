f841da2c934a0aabe3eb9822902b4eff954959a6
Josh <joshbbrown1007@gmail.com>
2026-02-22 20:58:10 -0700

    Added username and authstate to the app component

--- src/app.jsx
+++ src/app.jsx
@@ -9,6 +9,11 @@ import { Packs } from './packs/packs';
 import { Trades } from './trades/trades';
 
 export default function App() {
+
+  const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+  const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+  const [authState, setAuthState] = React.useState(currentAuthState);
+
   return (
     <BrowserRouter>
   
