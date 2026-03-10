68c4bcc845cc6452edddea7588469d90390bac2a
Josh <joshbbrown1007@gmail.com>
2026-03-10 16:52:16 -0600

    created login service file

---
+++ src/services/loginService.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const uuid = require('uuid');
+const bcrypt = require('bcryptjs');
+
+app.use(express.json());
+app.use(cookieParser());
